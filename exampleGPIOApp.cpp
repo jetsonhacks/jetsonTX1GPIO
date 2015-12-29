6a7204c43792dc48dced6b5f68ae4c6fb33b3184
jetsonhacks <jim@jetsonhacks.com>
2015-12-28 23:06:21 -0800

    Set different I/O pins

--- exampleGPIOApp.cpp
+++ exampleGPIOApp.cpp
@@ -40,8 +40,8 @@ int main(int argc, char *argv[]){
     cout << "Testing the GPIO Pins" << endl;
 
 
-    jetsonTX1GPIONumber redLED = gpio186 ;     // Ouput
-    jetsonTX1GPIONumber pushButton = gpio37 ; // Input
+    jetsonTX1GPIONumber redLED = gpio219 ;     // Ouput
+    jetsonTX1GPIONumber pushButton = gpio38 ; // Input
     // Make the button and led available in user space
     gpioExport(pushButton) ;
     gpioExport(redLED) ;
